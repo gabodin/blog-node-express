8b436dae47b54b2a1ba5bb9b90bcc2872c51986b
Victor Castro <vgabriel2211@gmail.com>
2022-02-25 15:43:40 -0300

    Adição do banco de dados

--- app.js
+++ app.js
@@ -1,19 +1,10 @@
-//jshint esversion:6
-
 const express = require("express");
 const bodyParser = require("body-parser");
+const mongoose = require('mongoose');
 const ejs = require("ejs");
 const { redirect } = require("express/lib/response");
 const _ = require('lodash');
-// const Post = require(__dirname + "/entities/Post.js");
-
-
-
-const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
-const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
-const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
-
-const posts = [];
+const {Posts} = require("./entities/Post");
 
 const app = express();
 
@@ -21,30 +12,47 @@ app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
+mongoose.connect("mongodb://localhost/blogDB");
 
 
-
-app.get("/", function(req, res) {
-  res.render("home", {posts: posts});
+app.get("/", async function(req, res) {
+  try {
+    const posts = await Posts.find();
+    res.render("home", {posts: posts});
+  }
+  catch(err) {
+    console.log(err.message);
+    res.render("not-found", {postTitle: "posts"});
+  }
 });
 
-app.get("/posts/:postTitle", function(req, res) {
+app.get("/posts/:postTitle", async function(req, res) {
   const postTitle = _.lowerCase(req.params.postTitle);
+  console.log(postTitle);
+  const foundPosts = await Posts.find({title: postTitle});
 
-  const postsFound = [];
-
-  posts.forEach( post => {
-    if(_.lowerCase(post.title) === postTitle) 
-      postsFound.push(post);
-  })
-
-  if(postsFound.length === 0) {
-    res.render("not-found", {postTitle: postTitle})
-  } 
-  else {
-    res.render("post", {posts: postsFound});
+  console.log(foundPosts);
+  if(foundPosts.length !==  0) {
+    res.render("post", {posts: foundPosts})
   }
+  else res.render("not-found", {postTitle: postTitle});
+
   
+});
+
+app.get("/details/:postId", async function(req, res) {
+  const postId = req.params.postId;
+  
+  try {
+    Posts.findOne({_id: postId}, function(err, foundPost) {
+
+      res.render("postDetails", {post: foundPost});
+    })
+  } catch(err) {
+    console.log(err.message);
+    res.render("not-found", {postTitle: "posts"});
+  }
+
 });
 
 app.get("/about", function(req, res) {
@@ -59,21 +67,26 @@ app.get("/compose", function(req, res) {
   res.render("compose", {});
 });
 
-app.post("/compose", function(req, res) {
-  const postTitle = req.body.postTitle;
+app.post("/compose", async function(req, res) {
+  const postTitle = _.lowerCase(req.body.postTitle);
   const postBody  = req.body.postContent;
 
-  const post = {
+  const post = new Posts({
     title: postTitle,
     body: postBody
-  }
+  });
 
-  posts.push(post);
-  res.redirect("/");
+  try {
+    const savedPost = await post.save();
+    console.log(savedPost);
+    res.redirect("/");
+  } 
+  catch(err) {
+    console.log(err.message);
+    res.render("not-found", {postTitle: "posts"});
+  } 
+  
 })
-
-
-
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
